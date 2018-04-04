0cb3a06b2ac6516d6d78f626ac5f0b6c7d541eab
surajdevesan <surajd@qburst.com>
2018-04-05 01:39:10 +0530

    Added events

---
+++ events.go
@@ -0,0 +1,9 @@
+package main
+
+func addToSpace(space SpaceStruct) string {
+	return "Thank you for adding me"
+}
+
+func messageFromUser() string {
+	return "I am under development and these features are not yet developed. You can track the progress <https://github.com/surajdevesan/testBot|here>	"
+}
--- main.go
+++ main.go
@@ -1,14 +1,53 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
+
+type SpaceStruct struct {
+	Name        string `json:"name"`
+	DisplayName string `json:"displayName"`
+	Type        string `json:"type"`
+}
+
+type Request struct {
+	Type    string      `json:"type"`
+	Token   string      `json:"token"`
+	Space   SpaceStruct `json:"space"`
+	Message struct {
+		Name   string `json:"name"`
+		Sender struct {
+			Name        string `json:"name"`
+			DisplayName string `json:"displayName"`
+			AvatarURL   string `json:"avatarUrl"`
+			Email       string `json:"email"`
+		} `json:"sender"`
+		Text   string `json:"text"`
+		Thread struct {
+			Name string `json:"name"`
+		} `json:"thread"`
+	} `json:"message"`
+}
 
 func main() {
 	r := gin.Default()
 	r.POST("/", func(c *gin.Context) {
+		data := &Request{}
+		response := "Test Default event"
+		c.Bind(data)
+		// x,_ := ioutil.ReadAll(c.Request.Body)
+		fmt.Println(data)
+		switch eventType := data.Type; eventType {
+		case "ADDED_TO_SPACE":
+			response = addToSpace(data.Space)
+		case "MESSAGE":
+			response = messageFromUser()
+		}
+		// fmt.Printf("%s", string(x))
 		c.JSON(200, gin.H{
-			"text": "Event Detected",
+			"text": response,
 		})
 	})
 	r.GET("/", func(c *gin.Context) {
